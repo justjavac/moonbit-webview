d736d46b4aa3a78fc610c99a82522f11a7cef69c
迷渡 <justjavac@gmail.com>
2025-05-28 15:44:36 +0800

    Add CMakeLists.txt for webview project and simplify build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,3 @@
-cd webview
-
-cmake -G "Ninja Multi-Config" -B build -S . \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DWEBVIEW_BUILD_TESTS=OFF \
-    -DWEBVIEW_BUILD_EXAMPLES=OFF \
-    -DWEBVIEW_USE_CLANG_TOOLS=OFF \
-    -DWEBVIEW_ENABLE_CHECKS=OFF \
-    -DWEBVIEW_USE_CLANG_TIDY=OFF \
-    -DWEBVIEW_BUILD_DOCS=OFF \
-    -DWEBVIEW_USE_CLANG_FORMAT=OFF
-
-cmake --build build --config Release
-
-cd ..
+cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release
+cmake --build build
+export DYLD_LIBRARY_PATH="$(pwd)/build/lib"
