872e137bd041f97c93af99cdcf85e79803758c40
迷渡 <justjavac@gmail.com>
2025-05-31 21:21:37 +0800

    Update CC_LINK_FLAGS to use relative path for library in env.bat

--- scripts/env.bat
+++ scripts/env.bat
@@ -3,4 +3,4 @@ set VS2022_DIR="C:\Program Files\Microsoft Visual Studio\2022\Community"
 call %VS2022_DIR%\VC\Auxiliary\Build\vcvarsall.bat x64
 set CC="cl"
 set CC_FLAGS=""
-set CC_LINK_FLAGS="/link /LIBPATH:D:\\Code\\moonbit-webview\\lib webview.lib"
+set CC_LINK_FLAGS="/link /LIBPATH:lib webview.lib"
